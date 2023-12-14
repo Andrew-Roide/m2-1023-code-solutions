b483ce79b36dbe3e3e6db7f476ebb8da4b157cdf
Andrew Roide <andrew.roide@hotmail.com>
2023-12-14 23:37:49 +0000

    added forgotten button-label from assignment

--- react-toggle-switch-component/my-app/src/App.tsx
+++ react-toggle-switch-component/my-app/src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 
 export default function App() {
   const [enabled, setEnabled] = useState(false);
+  const modifiedText = enabled ? 'on' : 'off';
+  const fontColor = {
+    color: modifiedText === 'on' ? 'white' : 'black',
+  };
 
   function handleClick() {
     document.body.style.backgroundColor = enabled ? 'white' : '#181818';
@@ -10,12 +14,17 @@ export default function App() {
   }
 
   return (
-    <div
-      className={`toggle-container ${enabled ? 'enabled' : ''}`}
-      onClick={handleClick}>
-      <div className="toggle-track">
-        <div className="toggle-thumb"></div>
+    <>
+      <div
+        className={`toggle-container ${enabled ? 'enabled' : ''}`}
+        onClick={handleClick}>
+        <div className="toggle-track">
+          <div className="toggle-thumb"></div>
+        </div>
       </div>
-    </div>
+      <div className="button-label" style={fontColor}>
+        {modifiedText}
+      </div>
+    </>
   );
 }
